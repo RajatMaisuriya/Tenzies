a6a83d0d1b63aeda759d4fa9ef6eb119ad18f45c
RajatMaisuriya <rajat.study05@gmail.com>
2025-10-18 17:20:01 -0400

    Dies page improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import Die from "./Die";
+import "./App.css";
 
 function App() {
   // const [count, setCount] = useState(0)
 
+  function generateAllDies() {
+    const newDie = [];
+    for (let i = 0; i < 10; i++) {
+      const rand = Math.ceil(Math.random() * 6);
+      newDie.push(rand);
+    }
+    return newDie;
+  }
+
+  console.log(generateAllDies());
   return (
     <>
-      <div className='gameEnvironment'>Let's Do something...</div>
+      <div className="gameEnvironment">
+        <div className="bt-css">
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Die.jsx
@@ -0,0 +1,4 @@
+import "./Die.css";
+export default function Die(props) {
+  return <button className="btn-css">{props.value}</button>;
+}
